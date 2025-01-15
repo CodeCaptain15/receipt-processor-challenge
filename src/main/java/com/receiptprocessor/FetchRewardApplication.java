f17d89db17f9a3d04e39a944f051b9cc14f565b8
dines <dineshpampati@gmail.com>
2025-01-15 13:40:45 -0600

    Init

--- src/main/java/com/receiptprocessor/FetchRewardApplication.java
+++ src/main/java/com/receiptprocessor/FetchRewardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FetchRewardApplication {
 
 	public static void main(String[] args) {
-		// Main method to run the Spring Boot 
+		// Main method to run the Spring Boot
 		SpringApplication.run(FetchRewardApplication.class, args);
 	}
 
