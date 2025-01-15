5fd720159b4676bfe82fda16079ceb8739b23aa8
CodeCaptain15 <dpampat@ilstu.edu>
2025-01-15 13:21:17 -0600

    Init

---
+++ src/test/java/com/receiptprocessor/FetchRewardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receiptprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchRewardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
