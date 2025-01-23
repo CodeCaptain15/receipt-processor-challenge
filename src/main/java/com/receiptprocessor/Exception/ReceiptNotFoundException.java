105846fb591e26f8fcb71d173a8ab168b3a39a61
dines <dpampat@ilstu.edu>
2025-01-22 18:02:22 -0600

    fixing some errors

--- src/main/java/com/receiptprocessor/Exception/ReceiptNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.receiptprocessor.Exception;
-
-public class ReceiptNotFoundException extends RuntimeException {
-    public ReceiptNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/receiptprocessor/Repository/ReceiptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.receiptprocessor.Repository;
-
-import com.receiptprocessor.Entity.Receipt;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ReceiptRepository extends JpaRepository<Receipt, UUID> {
-}
